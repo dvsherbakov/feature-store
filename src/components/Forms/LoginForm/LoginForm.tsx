ace8eabb647edd8faeb7617bb3b4424d212b2c3d
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-07-12 18:03:21 +0500

    some changes

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,14 +1,16 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import styles from './loginform.module.scss'
 import {GlowingButton} from "../../Buttons";
 import {useCoordinates} from "../../../hooks/useCoordinates";
-import {TCoordinates} from "../../../hooks/types";
 import {useResolveAddress} from "../../../hooks/useResolveAddress";
+import {TCoordinates} from "../../../hooks/types";
+import {TShortAddress} from "./types";
 
+import styles from './loginform.module.scss'
 
 export const LoginForm = () => {
     const coordinates = useCoordinates()
     const [cValue, setCValue] = useState<TCoordinates | null>(null)
+    const [addresses, setAddresses] = useState<TShortAddress[] | undefined>([])
     const address = useResolveAddress();
 
     const {lat, lng} = cValue ? cValue : {lat: null, lng: null};
@@ -17,9 +19,13 @@ export const LoginForm = () => {
         (async () => {
             if (lat && lng) {
                 const adr = await address(lat, lng)
-                console.log(adr?.data.filter(x => (!!x.number && !!x.street && !!x.country && !!x.region)).map(y => ({
-                    street: y.street, number: y.number, country: y.country, region: y.region
-                })))
+                setAddresses(adr?.data.filter(x => (!!x.number && !!x.street && !!x.country && !!x.region)).map(y => ({
+                    street: y.street,
+                    number: y.number,
+                    country: y.country,
+                    region: y.region,
+                    label: y.label
+                } as TShortAddress)))
             }
         })()
     }, [lat, lng, address])
@@ -38,6 +44,10 @@ export const LoginForm = () => {
             <input type={'text'} required={true}/>
             <span>Last Name</span>
         </div>
+        {!!addresses?.length && <div className={styles['inputBox']}>
+            <input type={"text"} required={true}/>
+            <span>Founded</span>
+        </div>}
         <GlowingButton title={'location'} color={'#0f3c4a'} key='LocationButton' onClick={buttonHandle}/>
     </div>);
 };
---
+++ src/components/Forms/LoginForm/types.ts
@@ -0,0 +1,7 @@
+export type TShortAddress = {
+    country: string
+    number: string | number
+    region: string
+    street: string
+    label?: string
+}
