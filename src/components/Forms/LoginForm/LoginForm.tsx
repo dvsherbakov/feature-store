45ccec1589eacb9cc8ac9c4f532b7170566a95d5
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-07-12 11:09:54 +0500

    optimize address query

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -17,28 +17,28 @@ export const LoginForm = () => {
         (async () => {
             if (lat && lng) {
                 const adr = await address(lat, lng)
-                console.log(adr?.data.filter(x => (!!x.number && !!x.street)))
+                console.log(adr?.data.filter(x => (!!x.number && !!x.street && !!x.country && !!x.region)).map(y => ({
+                    street: y.street, number: y.number, country: y.country, region: y.region
+                })))
             }
         })()
     }, [lat, lng, address])
 
     const buttonHandle = useCallback(async () => {
         const cords = await coordinates()
-        setCValue(cords)
-    }, [coordinates])
+        if (lng !== cords?.lng || lat !== cords?.lat) setCValue(cords)
+    }, [coordinates, lat, lng])
 
-    return (
-        <div className={styles['loginForm']}>
-            <div className={styles['inputBox']}>
-                <input type={'text'} required={true}/>
-                <span>First name</span>
-            </div>
-            <div className={styles['inputBox']}>
-                <input type={'text'} required={true}/>
-                <span>Last Name</span>
-            </div>
-            <GlowingButton title={'location'} color={'#0f3c4a'} key='LocationButton' onClick={buttonHandle}/>
+    return (<div className={styles['loginForm']}>
+        <div className={styles['inputBox']}>
+            <input type={'text'} required={true}/>
+            <span>First name</span>
         </div>
-    );
+        <div className={styles['inputBox']}>
+            <input type={'text'} required={true}/>
+            <span>Last Name</span>
+        </div>
+        <GlowingButton title={'location'} color={'#0f3c4a'} key='LocationButton' onClick={buttonHandle}/>
+    </div>);
 };
 
