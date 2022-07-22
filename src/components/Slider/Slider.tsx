9b06508788dce53770e2e602c1a06e823f61dead
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-07-22 12:30:26 +0500

    add slider indicator

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,7 +4,7 @@ import {usePictures} from "../../hooks/usePictures";
 import {CirclesLoader} from "../Loaders";
 
 export const Slider = () => {
-    const pictures = usePictures(5)
+    const pictures = usePictures(8)
     const [position, setPosition] = useState<number>(0)
     const handleLeft = () => {
         if (position > 0) setPosition(pos => --pos)
@@ -18,6 +18,11 @@ export const Slider = () => {
             <img src={URL.createObjectURL(pictures[position])} alt=''/>
             <button className={styles['left']} onClick={handleLeft}>{'<'}</button>
             <button className={styles['right']} onClick={handleRight}>{'>'}</button>
+            <div className={styles['indicators']}>
+                <ul>{Array.from(Array(pictures.length).keys()).map(itm => <li
+                    className={itm === position ? styles['selected'] : ''} key={itm.toString()}
+                    onClick={() => setPosition(itm)}></li>)}</ul>
+            </div>
         </div>)}
     </div>);
 };
