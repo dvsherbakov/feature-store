02f1595c223959b38dc0edff7f9d2ad2d935dbf9
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-08-25 20:00:36 +0300

    create month array

--- src/components/DataPicker/DataPicker.tsx
+++ src/components/DataPicker/DataPicker.tsx
@@ -2,6 +2,36 @@ import React from 'react';
 import styles from './datapicker.module.scss'
 
 export const DataPicker = () => {
+    const getMonthDays = (year: number, month: number) => {
+        let curDate = new Date(year, month, 1)
+        let result: Date[] = []
+
+        while (curDate.getMonth() === month) {
+            result.push(new Date(curDate))
+            curDate.setDate(curDate.getDate() + 1)
+        }
+
+
+        curDate = new Date(result[0])
+
+        while (curDate.getDay() !== 0) {
+            result = [new Date(curDate), ...result]
+            curDate.setDate(curDate.getDate() - 1)
+        }
+
+        curDate = new Date(result[result.length - 1])
+        curDate.setDate(curDate.getDate() + 1)
+        while (curDate.getDay() !== 1) {
+            result = [...result, new Date(curDate)]
+            curDate.setDate(curDate.getDate() + 1)
+        }
+
+        return result
+    }
+
+    const m = getMonthDays(2021, 7)
+    console.log(m)
+
     return (
         <div className={styles['dataPicker']}>
             Just Data Picker
---
+++ src/components/DataPicker/types.ts
@@ -0,0 +1,10 @@
+export enum daysOfWeek {
+    sunday = 'Воскресенье',
+    monday = 'Понедельник',
+    tuesday = 'Вторник',
+    wednesday = 'Среда',
+    thursday = 'Четверг',
+    friday = 'Пятница',
+    saturday = 'Суббота'
+}
+export type week = [number, number, number, number, number, number, number]
