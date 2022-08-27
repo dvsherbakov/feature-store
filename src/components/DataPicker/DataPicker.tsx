39010d06f2295c16f449dcbe1cc2bf50dad112c5
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-08-27 17:50:33 +0500

    dp first probe

--- src/components/DataPicker/DataPicker.tsx
+++ src/components/DataPicker/DataPicker.tsx
@@ -1,40 +1,19 @@
 import React from 'react';
 import styles from './datapicker.module.scss'
+import {getMonthDays} from "./getMonthDays";
 
 export const DataPicker = () => {
-    const getMonthDays = (year: number, month: number) => {
-        let curDate = new Date(year, month, 1)
-        let result: Date[] = []
-
-        while (curDate.getMonth() === month) {
-            result.push(new Date(curDate))
-            curDate.setDate(curDate.getDate() + 1)
-        }
-
-
-        curDate = new Date(result[0])
-
-        while (curDate.getDay() !== 0) {
-            result = [new Date(curDate), ...result]
-            curDate.setDate(curDate.getDate() - 1)
-        }
-
-        curDate = new Date(result[result.length - 1])
-        curDate.setDate(curDate.getDate() + 1)
-        while (curDate.getDay() !== 1) {
-            result = [...result, new Date(curDate)]
-            curDate.setDate(curDate.getDate() + 1)
-        }
-
-        return result
-    }
-
-    const m = getMonthDays(2021, 7)
-    console.log(m)
+    const month = 7
+    const m = getMonthDays(2021, month)
+    const dataViewer = m.map((itm, idx) => (
+        <div key={idx.toString()}
+             className={[styles['dayViewer'], [itm.getDay() === 0 ? styles['red'] : '']].join(' ')}>
+            {itm.getDate()}
+        </div>))
 
     return (
         <div className={styles['dataPicker']}>
-            Just Data Picker
+            <div className={styles['dataViewer']}> {dataViewer} </div>
         </div>
     );
 };
