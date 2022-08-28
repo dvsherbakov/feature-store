bfc996baed6a54e13fe1758a5024aba7672723ea
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-08-28 23:05:33 +0500

    switch date selector

--- src/components/DataPicker/DataPicker.tsx
+++ src/components/DataPicker/DataPicker.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import styles from './datapicker.module.scss'
 import {getMonthDays} from "./getMonthDays";
 import {months} from "./types";
@@ -6,6 +6,14 @@ import {months} from "./types";
 export const DataPicker = () => {
     const [month, setMonth] = useState<number>((new Date()).getMonth())
     const [selectedDate, setSelectedDate] = useState<Date>(new Date())
+    const [visible, setVisible] = useState<boolean>(false)
+
+    useEffect(() => {
+        if (visible)
+            setTimeout(() => {
+                setVisible(false)
+            }, 5000)
+    }, [selectedDate]) // eslint-disable-line react-hooks/exhaustive-deps
 
     const handleNextMonth = useCallback(() => {
         setMonth(month === 11 ? 0 : (cur) => cur + 1)
@@ -14,6 +22,10 @@ export const DataPicker = () => {
     const handlePrevMonth = useCallback(() => {
         setMonth(month === 0 ? 11 : (cur) => cur - 1)
     }, [month])
+
+    const visibleSwitcher = useCallback(() => {
+        setVisible((v) => !v)
+    }, [])
 
     const m = getMonthDays(2022, month)
 
@@ -25,14 +37,18 @@ export const DataPicker = () => {
         {itm.getDate()}
     </div>))
 
-    return (<div className={styles['dataPicker']}>
-        <div className={styles['selectedViewer']}>{selectedDate.toLocaleDateString()}</div>
-        <div className={styles['monthArea']}>
-            <button onClick={handlePrevMonth}>{'<'}</button>
-            {months[month]}
-            <button onClick={handleNextMonth}>{'>'}</button>
+    return (<div className={styles['datePicker']}>
+        <div className={styles['selectedViewer']}>
+            {selectedDate.toLocaleDateString()}
+            <button className={styles[visible ? 'marked' : '']} onClick={visibleSwitcher}>{'>'}</button>
         </div>
-
-        <div className={styles['dataViewer']}> {dataViewer} </div>
+        {visible && <section>
+            <div className={styles['monthArea']}>
+                <button onClick={handlePrevMonth}>{'<'}</button>
+                {months[month]}
+                <button onClick={handleNextMonth}>{'>'}</button>
+            </div>
+            <div className={styles['dataViewer']}> {dataViewer} </div>
+        </section>}
     </div>);
 };
