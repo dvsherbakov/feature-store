638b080a9a6d6435b776de09d5f88d3e9c5b7765
Dmitriy Scherbakov <dmitriy.scherbakov@ocrv.ru>
2022-08-31 17:43:44 +0500

    add year in datapicker

---
+++ src/components/SvgShapes/Shapes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const FillArrowUp = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="10" width="20">
+        <polygon points="2,8 10,2 18,8" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>);
+
+export const FillArrowDown = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="10" width="20">
+        <polygon points="18,2 10,8 2,2"  strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>);
