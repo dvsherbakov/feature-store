0b2e837745c3ed7a2f68c5aa9f8912d58b2c876d
Dmitriy Scherbakov <dmitriy.scherbakov@ocrv.ru>
2022-09-07 12:42:43 +0500

    add playwright config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {chromium} from "playwright";
+import {toMatchImageSnapshot} from "jest-image-snapshot";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+expect.extend({toMatchImageSnapshot});
+
+describe("screenshots", () => {
+    test("colored-form page", async () => {
+        const browser = await chromium.launch({headless: true, args: ["--disable-web-security"]});
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("http://localhost:3000/colored-form");
+
+        await page.locator("footer").waitFor();
+
+        const image = await page.screenshot({fullPage: true});
+        expect(image).toMatchImageSnapshot({
+            failureThreshold: 0.01,
+            failureThresholdType: "percent",
+        });
+
+        await browser.close();
+    });
+
+    test("awesome page", async () => {
+        const browser = await chromium.launch({headless: true, args: ["--disable-web-security"]});
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto("http://localhost:3000/awesome");
+
+        await page.locator("footer").waitFor();
+
+        const image = await page.screenshot({fullPage: true});
+        expect(image).toMatchImageSnapshot({
+            failureThreshold: 0.01,
+            failureThresholdType: "percent",
+        });
+
+        await browser.close();
+    });
 });
