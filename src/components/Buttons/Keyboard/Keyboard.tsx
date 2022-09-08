bc73e273a5092f86e6ed3313c980d72fe16949eb
Dmitriy Scherbakov <dmitriy.scherbakov@ocrv.ru>
2022-09-08 11:47:11 +0500

    refactor kbd

--- src/components/Buttons/Keyboard/Keyboard.tsx
+++ src/components/Buttons/Keyboard/Keyboard.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useMemo} from "react";
 import styles from "./keyboard.module.scss";
 
 const Keyboard = () => {
-    const rows = [
+    const rows = useMemo(() => [
         "qwertyuiop", "asdfghjkl", "zxcvbnm",
-    ].map((row) => (<div>
-            {row.split("").map((btn) => <span><i>{btn}</i></span>)}
+    ].map((row, idx) => (<div key={`row_${idx}`}>
+            {row.split("").map((btn) => <span key={`key_${btn}`} onClick={() => {
+                console.log(btn);
+            }}><i>{btn}</i></span>)}
         </div>),
-    );
+    ), []);
+
     return (
         <div className={styles["keyboard"]}>
             {rows}
