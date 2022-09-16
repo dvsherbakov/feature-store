a7ce3b29ed3ff76d5fed45338a1347c91c478e60
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-09-16 16:49:21 +0500

    change to real test pw

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,31 +1,36 @@
-import type {PlaywrightTestConfig} from "@playwright/test";
-import {devices} from "@playwright/test";
-
+import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
-    testDir: "./tests",
-    timeout: 30 * 1000,
-    expect: {
-        timeout: 5000,
-    },
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: "html",
-    use: {
-        actionTimeout: 0,
-        trace: "on-first-retry",
-    },
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
 
-    projects: [
-        {
-            name: "chromium",
-            use: {
-                ...devices["Desktop Chrome"],
-            },
-        },
-    ],
-};
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+  webServer: {
+    command: 'npm run start',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: true,
+  },
+}
 
-export default config;
+export default config
--- src/components/Forms/ColoredForm/ColoredForm.tsx
+++ src/components/Forms/ColoredForm/ColoredForm.tsx
@@ -5,12 +5,12 @@ export const ColoredForm = () => {
     return (
         <main className={styles['colored']}>
             <div className={styles['inputBox']}>
-                <input type='text' required/>
+                <input type='text' name='firstName' required/>
                 <span>First Name</span>
                 <i/>
             </div>
             <div className={styles['inputBox']}>
-                <input type='text' required/>
+                <input type='text' name='lastName' required/>
                 <span>Last Name</span>
                 <i/>
             </div>
