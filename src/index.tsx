6da4b40b1e4fb3d55a9b11738b694c9de64ae561
Dmitry Sherbakov <dvsherbakov@gmail.com>
2022-09-27 21:09:47 +0500

    add react-buddy

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+  <BrowserRouter>
+    <DevSupport ComponentPreviews={ComponentPreviews}
+                useInitialHook={useInitial}
+    >
+      <App />
+    </DevSupport>
+  </BrowserRouter>
 );
 
